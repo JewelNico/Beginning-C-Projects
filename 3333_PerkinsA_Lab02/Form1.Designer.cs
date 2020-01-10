7e6b9ecd001c9e61cd08c8e46d5ee66a7abb28e3
Amber Perkins <amberperk87@gmail.com>
2020-01-10 14:17:49 -0600

    Add files via upload

---
+++ 3333_PerkinsA_Lab02/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace _3333_PerkinsA_Lab02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.threeOfClubsPictureBox = new System.Windows.Forms.PictureBox();
+            this.aceOfDiamondsPictureBox = new System.Windows.Forms.PictureBox();
+            this.queenOfHeartsPictureBox = new System.Windows.Forms.PictureBox();
+            this.sevenOfSpadesPictureBox = new System.Windows.Forms.PictureBox();
+            this.tenOfDiamondsPictureBox = new System.Windows.Forms.PictureBox();
+            this.instructionLabel = new System.Windows.Forms.Label();
+            this.cardLabel = new System.Windows.Forms.Label();
+            this.backFacePictureBox1 = new System.Windows.Forms.PictureBox();
+            this.backFacePictureBox2 = new System.Windows.Forms.PictureBox();
+            this.backFacePictureBox3 = new System.Windows.Forms.PictureBox();
+            this.backFacePictureBox4 = new System.Windows.Forms.PictureBox();
+            this.backFacePictureBox5 = new System.Windows.Forms.PictureBox();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.threeOfClubsPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aceOfDiamondsPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.queenOfHeartsPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sevenOfSpadesPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tenOfDiamondsPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.backFacePictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.backFacePictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.backFacePictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.backFacePictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.backFacePictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // threeOfClubsPictureBox
+            // 
+            this.threeOfClubsPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("threeOfClubsPictureBox.Image")));
+            this.threeOfClubsPictureBox.Location = new System.Drawing.Point(46, 96);
+            this.threeOfClubsPictureBox.Name = "threeOfClubsPictureBox";
+            this.threeOfClubsPictureBox.Size = new System.Drawing.Size(100, 140);
+            this.threeOfClubsPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.threeOfClubsPictureBox.TabIndex = 4;
+            this.threeOfClubsPictureBox.TabStop = false;
+            this.threeOfClubsPictureBox.Click += new System.EventHandler(this.threeOfClubsPictureBox_Click);
+            // 
+            // aceOfDiamondsPictureBox
+            // 
+            this.aceOfDiamondsPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("aceOfDiamondsPictureBox.Image")));
+            this.aceOfDiamondsPictureBox.Location = new System.Drawing.Point(470, 96);
+            this.aceOfDiamondsPictureBox.Name = "aceOfDiamondsPictureBox";
+            this.aceOfDiamondsPictureBox.Size = new System.Drawing.Size(100, 140);
+            this.aceOfDiamondsPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.aceOfDiamondsPictureBox.TabIndex = 5;
+            this.aceOfDiamondsPictureBox.TabStop = false;
+            this.aceOfDiamondsPictureBox.Click += new System.EventHandler(this.aceOfDiamondsPictureBox_Click);
+            // 
+            // queenOfHeartsPictureBox
+            // 
+            this.queenOfHeartsPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("queenOfHeartsPictureBox.Image")));
+            this.queenOfHeartsPictureBox.Location = new System.Drawing.Point(364, 96);
+            this.queenOfHeartsPictureBox.Name = "queenOfHeartsPictureBox";
+            this.queenOfHeartsPictureBox.Size = new System.Drawing.Size(100, 140);
+            this.queenOfHeartsPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.queenOfHeartsPictureBox.TabIndex = 6;
+            this.queenOfHeartsPictureBox.TabStop = false;
+            this.queenOfHeartsPictureBox.Click += new System.EventHandler(this.queenOfHeartsPictureBox_Click);
+            // 
+            // sevenOfSpadesPictureBox
+            // 
+            this.sevenOfSpadesPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("sevenOfSpadesPictureBox.Image")));
+            this.sevenOfSpadesPictureBox.Location = new System.Drawing.Point(258, 96);
+            this.sevenOfSpadesPictureBox.Name = "sevenOfSpadesPictureBox";
+            this.sevenOfSpadesPictureBox.Size = new System.Drawing.Size(100, 140);
+            this.sevenOfSpadesPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.sevenOfSpadesPictureBox.TabIndex = 7;
+            this.sevenOfSpadesPictureBox.TabStop = false;
+            this.sevenOfSpadesPictureBox.Click += new System.EventHandler(this.sevenOfSpadesPictureBox_Click);
+            // 
+            // tenOfDiamondsPictureBox
+            // 
+            this.tenOfDiamondsPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("tenOfDiamondsPictureBox.Image")));
+            this.tenOfDiamondsPictureBox.Location = new System.Drawing.Point(152, 96);
+            this.tenOfDiamondsPictureBox.Name = "tenOfDiamondsPictureBox";
+            this.tenOfDiamondsPictureBox.Size = new System.Drawing.Size(100, 140);
+            this.tenOfDiamondsPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.tenOfDiamondsPictureBox.TabIndex = 8;
+            this.tenOfDiamondsPictureBox.TabStop = false;
+            this.tenOfDiamondsPictureBox.Click += new System.EventHandler(this.tenOfDiamondsPictureBox_Click);
+            // 
+            // instructionLabel
+            // 
+            this.instructionLabel.AutoSize = true;
+            this.instructionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.instructionLabel.Location = new System.Drawing.Point(224, 25);
+            this.instructionLabel.Name = "instructionLabel";
+            this.instructionLabel.Size = new System.Drawing.Size(240, 20);
+            this.instructionLabel.TabIndex = 9;
+            this.instructionLabel.Text = "Click a Card to See Its Name";
+            // 
+            // cardLabel
+            // 
+            this.cardLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.cardLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.cardLabel.Location = new System.Drawing.Point(152, 310);
+            this.cardLabel.Name = "cardLabel";
+            this.cardLabel.Size = new System.Drawing.Size(322, 20);
+            this.cardLabel.TabIndex = 11;
+            this.cardLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // backFacePictureBox1
+            // 
+            this.backFacePictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("backFacePictureBox1.Image")));
+            this.backFacePictureBox1.Location = new System.Drawing.Point(46, 96);
+            this.backFacePictureBox1.Name = "backFacePictureBox1";
+            this.backFacePictureBox1.Size = new System.Drawing.Size(100, 140);
+            this.backFacePictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.backFacePictureBox1.TabIndex = 12;
+            this.backFacePictureBox1.TabStop = false;
+            this.backFacePictureBox1.Click += new System.EventHandler(this.backFace1_Click);
+            // 
+            // backFacePictureBox2
+            // 
+            this.backFacePictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("backFacePictureBox2.Image")));
+            this.backFacePictureBox2.Location = new System.Drawing.Point(152, 96);
+            this.backFacePictureBox2.Name = "backFacePictureBox2";
+            this.backFacePictureBox2.Size = new System.Drawing.Size(100, 140);
+            this.backFacePictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.backFacePictureBox2.TabIndex = 13;
+            this.backFacePictureBox2.TabStop = false;
+            this.backFacePictureBox2.Click += new System.EventHandler(this.backFace2_Click);
+            // 
+            // backFacePictureBox3
+            // 
+            this.backFacePictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("backFacePictureBox3.Image")));
+            this.backFacePictureBox3.Location = new System.Drawing.Point(258, 96);
+            this.backFacePictureBox3.Name = "backFacePictureBox3";
+            this.backFacePictureBox3.Size = new System.Drawing.Size(100, 140);
+            this.backFacePictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.backFacePictureBox3.TabIndex = 14;
+            this.backFacePictureBox3.TabStop = false;
+            this.backFacePictureBox3.Click += new System.EventHandler(this.backFace3_Click);
+            // 
+            // backFacePictureBox4
+            // 
+            this.backFacePictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("backFacePictureBox4.Image")));
+            this.backFacePictureBox4.Location = new System.Drawing.Point(364, 96);
+            this.backFacePictureBox4.Name = "backFacePictureBox4";
+            this.backFacePictureBox4.Size = new System.Drawing.Size(100, 140);
+            this.backFacePictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.backFacePictureBox4.TabIndex = 15;
+            this.backFacePictureBox4.TabStop = false;
+            this.backFacePictureBox4.Click += new System.EventHandler(this.backFace4_Click);
+            // 
+            // backFacePictureBox5
+            // 
+            this.backFacePictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("backFacePictureBox5.Image")));
+            this.backFacePictureBox5.Location = new System.Drawing.Point(470, 96);
+            this.backFacePictureBox5.Name = "backFacePictureBox5";
+            this.backFacePictureBox5.Size = new System.Drawing.Size(100, 140);
+            this.backFacePictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.backFacePictureBox5.TabIndex = 16;
+            this.backFacePictureBox5.TabStop = false;
+            this.backFacePictureBox5.Click += new System.EventHandler(this.backFace5_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.resetButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resetButton.Location = new System.Drawing.Point(143, 378);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(75, 23);
+            this.resetButton.TabIndex = 17;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.exitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.Location = new System.Drawing.Point(399, 378);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 18;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.backFacePictureBox5);
+            this.Controls.Add(this.backFacePictureBox4);
+            this.Controls.Add(this.backFacePictureBox3);
+            this.Controls.Add(this.backFacePictureBox2);
+            this.Controls.Add(this.backFacePictureBox1);
+            this.Controls.Add(this.cardLabel);
+            this.Controls.Add(this.instructionLabel);
+            this.Controls.Add(this.tenOfDiamondsPictureBox);
+            this.Controls.Add(this.sevenOfSpadesPictureBox);
+            this.Controls.Add(this.queenOfHeartsPictureBox);
+            this.Controls.Add(this.aceOfDiamondsPictureBox);
+            this.Controls.Add(this.threeOfClubsPictureBox);
+            this.Name = "Form1";
+            this.Text = "Card Identifer";
+            ((System.ComponentModel.ISupportInitialize)(this.threeOfClubsPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aceOfDiamondsPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.queenOfHeartsPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sevenOfSpadesPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tenOfDiamondsPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.backFacePictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.backFacePictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.backFacePictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.backFacePictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.backFacePictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox threeOfClubsPictureBox;
+        private System.Windows.Forms.PictureBox aceOfDiamondsPictureBox;
+        private System.Windows.Forms.PictureBox queenOfHeartsPictureBox;
+        private System.Windows.Forms.PictureBox sevenOfSpadesPictureBox;
+        private System.Windows.Forms.PictureBox tenOfDiamondsPictureBox;
+        private System.Windows.Forms.Label instructionLabel;
+        private System.Windows.Forms.Label cardLabel;
+        private System.Windows.Forms.PictureBox backFacePictureBox1;
+        private System.Windows.Forms.PictureBox backFacePictureBox2;
+        private System.Windows.Forms.PictureBox backFacePictureBox3;
+        private System.Windows.Forms.PictureBox backFacePictureBox4;
+        private System.Windows.Forms.PictureBox backFacePictureBox5;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ 3333_PerkinsA_Lab02/Form1.cs
@@ -0,0 +1,126 @@
+﻿// Amber Perkins
+// 000828993
+// 09/17/2019
+// 09/20/2019
+// This program allows the user to toggle through five cards to reveal it's face value and name. 
+// This program also allows the user to reset the program to it's original state and the ability to exit.
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3333_PerkinsA_Lab02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void threeOfClubsPictureBox_Click(object sender, EventArgs e)
+        {
+            backFacePictureBox1.Visible = true;
+            cardLabel.Text = "";
+        }
+
+        private void tenOfDiamondsPictureBox_Click(object sender, EventArgs e)
+        {
+            backFacePictureBox2.Visible = true;
+            cardLabel.Text = "";
+        }
+
+        private void sevenOfSpadesPictureBox_Click(object sender, EventArgs e)
+        {
+            backFacePictureBox3.Visible = true;
+            cardLabel.Text = "";
+        }
+
+        private void queenOfHeartsPictureBox_Click(object sender, EventArgs e)
+        {
+            backFacePictureBox4.Visible = true;
+            cardLabel.Text = "";
+        }
+
+        private void aceOfDiamondsPictureBox_Click(object sender, EventArgs e)
+        {
+            backFacePictureBox5.Visible = true;
+            cardLabel.Text = "";
+        }
+
+        private void backFace1_Click(object sender, EventArgs e)
+        {
+            threeOfClubsPictureBox.Visible = true;
+            cardLabel.Text = "Three of Clubs";
+            backFacePictureBox1.Visible = false;
+            backFacePictureBox3.Visible = true;
+            backFacePictureBox2.Visible = true;
+            backFacePictureBox4.Visible = true;
+            backFacePictureBox5.Visible = true;
+        }
+
+        private void backFace2_Click(object sender, EventArgs e)
+        {
+            tenOfDiamondsPictureBox.Visible = true;
+            cardLabel.Text = "Ten of Diamonds";
+            backFacePictureBox2.Visible = false;
+            backFacePictureBox1.Visible = true;
+            backFacePictureBox3.Visible = true;
+            backFacePictureBox4.Visible = true;
+            backFacePictureBox5.Visible = true;
+        }
+
+        private void backFace3_Click(object sender, EventArgs e)
+        {
+            sevenOfSpadesPictureBox.Visible = true;
+            cardLabel.Text = "Seven of Spades";
+            backFacePictureBox3.Visible = false;
+            backFacePictureBox1.Visible = true;
+            backFacePictureBox2.Visible = true;
+            backFacePictureBox4.Visible = true;
+            backFacePictureBox5.Visible = true;
+        }
+
+        private void backFace4_Click(object sender, EventArgs e)
+        {
+            queenOfHeartsPictureBox.Visible = true;
+            cardLabel.Text = "Queen of Hearts";
+            backFacePictureBox4.Visible = false;
+            backFacePictureBox3.Visible = true;
+            backFacePictureBox2.Visible = true;
+            backFacePictureBox5.Visible = true;
+            backFacePictureBox1.Visible = true; 
+
+        }
+
+        private void backFace5_Click(object sender, EventArgs e)
+        {
+            aceOfDiamondsPictureBox.Visible = true;
+            cardLabel.Text = "Ace of Diamonds";
+            backFacePictureBox5.Visible = false;
+            backFacePictureBox3.Visible = true;
+            backFacePictureBox4.Visible = true;
+            backFacePictureBox2.Visible = true;
+            backFacePictureBox1.Visible = true;
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            backFacePictureBox1.Visible = true;
+            backFacePictureBox2.Visible = true;
+            backFacePictureBox3.Visible = true;
+            backFacePictureBox4.Visible = true;
+            backFacePictureBox5.Visible = true;
+            cardLabel.Text = "";
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
